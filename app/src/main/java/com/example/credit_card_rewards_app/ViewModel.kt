7bc6410fc35b366117f0582a4af0c59cd1e635ac
DevBhagat2004 <bhagatdev515@gmail.com>
2026-08-07 14:00:30 -0500

    Updated view model

--- app/src/main/java/com/example/credit_card_rewards_app/ViewModel.kt
+++ app/src/main/java/com/example/credit_card_rewards_app/ViewModel.kt
@@ -2,9 +2,18 @@ package com.example.credit_card_rewards_app
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class ViewModel(application: Application): AndroidViewModel(application)  {
-    fun findBest (RewardsNames: List<String>){
-        getMaxValue()
+    private val db = AppDatabase.getDatabase(application)
+    val cardDao = db.cardDao
+    val rewardDao = db.rewardDao
+
+    fun getBest (RewardsNames: String){
+        viewModelScope.launch {
+            val cardIds = rewardDao.getMaxValue(RewardsNames)
+            cardDao.selectCard(cardIds)
+        }
     }
 }
